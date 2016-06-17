f9c10de3380a088344c735e95d9a6f456da03993
龙小刚 <520longgang@163.com>
2016-06-17 08:59:53 +0800

    Initial Commit

---
+++ JRDdc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRDdc
+//
+//  Created by mac on 16/6/17.
+//  Copyright © 2016年 JR-LXG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JRDdc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JRDdc
+//
+//  Created by mac on 16/6/17.
+//  Copyright © 2016年 JR-LXG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
